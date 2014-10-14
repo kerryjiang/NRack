410ba8dddc300bc8daeeab261c04568f097ca293
Kerry Jiang <kerry-jiang@hotmail.com>
2014-10-14 01:11:04 -0700

    improved the configuration API

--- src/NDock.Base/Configuration/NDockConfigSection.cs
+++ src/NDock.Base/Configuration/NDockConfigSection.cs
@@ -1,9 +1,10 @@
-﻿using NDock.Base.Config;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Configuration;
 using System.Linq;
 using System.Text;
+using NDock.Base.Config;
 
 namespace NDock.Base.Configuration
 {
@@ -33,6 +34,63 @@ namespace NDock.Base.Configuration
         IEnumerable<IServerConfig> IConfigurationSource.Servers
         {
             get { return this.Servers; }
+        }
+
+        /// <summary>
+        /// Gets the option elements.
+        /// </summary>
+        public NameValueCollection OptionElements { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether an unknown element is encountered during deserialization.
+        /// To keep compatible with old configuration
+        /// </summary>
+        /// <param name="elementName">The name of the unknown subelement.</param>
+        /// <param name="reader">The <see cref="T:System.Xml.XmlReader"/> being used for deserialization.</param>
+        /// <returns>
+        /// true when an unknown element is encountered while deserializing; otherwise, false.
+        /// </returns>
+        /// <exception cref="T:System.Configuration.ConfigurationErrorsException">The element identified by <paramref name="elementName"/> is locked.- or -One or more of the element's attributes is locked.- or -<paramref name="elementName"/> is unrecognized, or the element has an unrecognized attribute.- or -The element has a Boolean attribute with an invalid value.- or -An attempt was made to deserialize a property more than once.- or -An attempt was made to deserialize a property that is not a valid member of the element.- or -The element cannot contain a CDATA or text element.</exception>
+        protected override bool OnDeserializeUnrecognizedElement(string elementName, System.Xml.XmlReader reader)
+        {
+            if (OptionElements == null)
+                OptionElements = new NameValueCollection();
+
+            OptionElements.Add(elementName, reader.ReadOuterXml());
+            return true;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether an unknown attribute is encountered during deserialization.
+        /// </summary>
+        /// <param name="name">The name of the unrecognized attribute.</param>
+        /// <param name="value">The value of the unrecognized attribute.</param>
+        /// <returns>
+        /// true when an unknown attribute is encountered while deserializing; otherwise, false.
+        /// </returns>
+        protected override bool OnDeserializeUnrecognizedAttribute(string name, string value)
+        {
+            const string xmlns = "xmlns";
+            const string xmlnsPrefix = "xmlns:";
+            const string xsiPrefix = "xsi:";
+
+            //for configuration intellisense, allow these unrecognized attributes: xmlns, xmlns:*, xsi:*
+            if (name.Equals(xmlns) || name.StartsWith(xmlnsPrefix) || name.StartsWith(xsiPrefix))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the child config.
+        /// </summary>
+        /// <typeparam name="TConfig">The type of the config.</typeparam>
+        /// <param name="childConfigName">Name of the child config.</param>
+        /// <returns></returns>
+        public TConfig GetChildConfig<TConfig>(string childConfigName)
+            where TConfig : ConfigurationElement, new()
+        {
+            return this.OptionElements.GetChildConfig<TConfig>(childConfigName);
         }
     }
 
---
+++ src/NDock.Base/Environment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDock.Base
+{
+    /// <summary>
+    /// System environment variables of NDock
+    /// </summary>
+    public static class Environment
+    {
+        /// <summary>
+        /// Is this runtime Mono
+        /// </summary>
+        public static readonly bool IsMono;
+
+        static Environment()
+        {
+            // detect the runtime by the type Mono.Runtime
+            IsMono = Type.GetType("Mono.Runtime") != null;
+        }
+    }
+}
