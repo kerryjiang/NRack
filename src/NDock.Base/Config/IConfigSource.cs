78d2a2b2532fc65c3de34c5fd418a104153867f8
Kerry Jiang <kerry-jiang@hotmail.com>
2015-04-25 12:13:40 +0800

    added missed files

---
+++ src/NDock.Base/Config/IConfigSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDock.Base.Config
+{
+    public interface IConfigSource
+    {
+        IsolationMode Isolation { get; }
+
+        IEnumerable<IServerConfig> Servers { get; }
+    }
+}
