e9ed586e32362f9e1cb262c600d7b2c6a0622868
Kerry Jiang <kerry-jiang@hotmail.com>
2015-05-10 00:55:20 +0800

    added MarshalManagedApp.cs

---
+++ src/NDock.Server/Isolation/MarshalManagedApp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NDock.Base;
+using NDock.Base.Config;
+using NDock.Base.Metadata;
+
+namespace NDock.Server.Isolation
+{
+    /// <summary>
+    /// Marshal wrap for managed app instance
+    /// We don't want to make the real app instance marshalable
+    /// </summary>
+    class MarshalManagedApp : MarshalByRefObject, IManagedApp
+    {
+        private IManagedApp m_ManagedApp;
+
+        public MarshalManagedApp(string appTypeName)
+        {
+            var appType = Type.GetType(appTypeName);
+            m_ManagedApp = (IManagedApp)Activator.CreateInstance(appType);
+        }
+
+        public string Name
+        {
+            get { return m_ManagedApp.Name; }
+        }
+
+        public bool Setup(IServerConfig config)
+        {
+            return m_ManagedApp.Setup(config);
+        }
+
+        public ServerState State
+        {
+            get { return m_ManagedApp.State; }
+        }
+
+        public IServerConfig Config
+        {
+            get { return m_ManagedApp.Config; }
+        }
+
+        public AppServerMetadataAttribute GetMetadata()
+        {
+            return m_ManagedApp.GetMetadata();
+        }
+
+        public bool Start()
+        {
+            return m_ManagedApp.Start();
+        }
+
+        public void Stop()
+        {
+            m_ManagedApp.Stop();
+        }
+    }
+}
