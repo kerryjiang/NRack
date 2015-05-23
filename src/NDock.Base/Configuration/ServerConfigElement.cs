89d539a71bc48d835bbb2feb637f12521b185f85
Kerry Jiang <kerry-jiang@hotmail.com>
2015-05-23 20:54:36 +0800

    added get child config api

--- src/NDock.Base/Configuration/ServerConfigElement.cs
+++ src/NDock.Base/Configuration/ServerConfigElement.cs
@@ -26,5 +26,17 @@ namespace NDock.Base.Configuration
         {
             get { return this["logFactory"] as string; }
         }
+
+        /// <summary>
+        /// Gets the child config.
+        /// </summary>
+        /// <typeparam name="TConfig">The type of the config.</typeparam>
+        /// <param name="childConfigName">Name of the child config.</param>
+        /// <returns></returns>
+        public TConfig GetChildConfig<TConfig>(string childConfigName)
+            where TConfig : ConfigurationElement, new()
+        {
+            return this.OptionElements.GetChildConfig<TConfig>(childConfigName);
+        }
     }
 }
