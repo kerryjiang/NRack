942e207e24aeb004b69e303a7e359517e26f646f
Kerry Jiang <kerry-jiang@hotmail.com>
2015-08-31 11:20:04 +0800

    added global assembly info file

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("NDock")]
+[assembly: AssemblyProduct("NDock")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
