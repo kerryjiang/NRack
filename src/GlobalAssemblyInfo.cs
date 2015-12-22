3cc924d58ba46be8e04412ef79849af3b4956aa6
Kerry Jiang <kerry-jiang@hotmail.com>
2015-12-22 20:07:03 +0800

    updated nuspec

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.1.1.3")]
-[assembly: AssemblyFileVersion("0.1.1.3")]
+[assembly: AssemblyVersion("0.1.1.4")]
+[assembly: AssemblyFileVersion("0.1.1.4")]
