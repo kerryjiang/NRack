774b0914660f7878ce7fa990bea0ff00125e9186
Kerry Jiang <kerry-jiang@hotmail.com>
2016-03-16 22:49:57 +0800

    test commit by ssh

--- src/Build.bat
+++ src/Build.bat
@@ -3,6 +3,7 @@
 set fdir=%ProgramFiles(x86)%\MSBuild\14.0
 set msbuild="%fdir%\bin\msbuild.exe"
 
+
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 %msbuild% NDock.sln /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\bin\Net40
 
